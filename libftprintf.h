7341b82db7ae15a3e6a769a73c425c0bc9dbb7b8
solmyr <alexey0gladyshev@gmail.com>
2021-06-27 15:01:43 +0300

    Add basic conversion tests

--- libftprintf.h
+++ libftprintf.h
@@ -1,6 +1,9 @@
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+
 int ft_printf(const char *, ...);
 
 #endif
--- tests.c
+++ tests.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #include "libftprintf.h"
+/*
+int	run_test(char *str, ...)
+{
+	int res;
+	int ft_res;
+	va_list pargs;
+
+
+
+	ft_putstr_fd(str)
+	res = printf(str, "Hello");
+	ft_res = ft_printf(str, "Hello");
+	if (res == ft_res)
+		printf("RETURN EQUAL\n");
+	else
+		printf("RETURN DIFF\n");
+}
+*/
 
 int	main(void)
 {
+	int i;
+
+	i = 15;
 	printf("Test basic conversions\n");
-	printf("OG: %s\n", "string");
-	ft_printf("FT: %s\n", "string");
+	if (printf("String:		|%s|\n", "Hello") != ft_printf("String ft:	|%s|\n", "Hello"))
+		printf("RETURN DIFF\n");
+	if (printf("Int d:		|%d|\n", i) != ft_printf("Int ft:		|%d|\n", i))
+		printf("RETURN DIFF\n");
+	if (printf("Int i:		|%i|\n", i) != ft_printf("Int i ft:	|%i|\n", i))
+		printf("RETURN DIFF\n");
+	if (printf("Char:		|%c|\n", 'A') != ft_printf("Char ft:	|%c|\n", 'A'))
+		printf("RETURN DIFF\n");
+	if (printf("Address:	|%p|\n", &i) != ft_printf("Address ft:	|%p|\n", &i))
+		printf("RETURN DIFF\n");
+	if (printf("Unsigned:	|%u|\n", (unsigned int)i) != ft_printf("Unsigned ft:	|%p|\n", (unsigned int)i))
+		printf("RETURN DIFF\n");
+	if (printf("hex:		|%x|\n", (unsigned int)i) != ft_printf("hex ft:		|%x|\n", (unsigned int)i))
+		printf("RETURN DIFF\n");
+	if (printf("HEX:		|%X|\n", (unsigned int)i) != ft_printf("HEX FT:		|%X|\n", (unsigned int)i))
+		printf("RETURN DIFF\n");
+	if (printf("ESCAPE:		|%%|\n") != ft_printf("ESCAPE FT:	|%%|\n"))
+		printf("RETURN DIFF\n");
 	return (0);
 }
